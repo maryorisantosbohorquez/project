d1db8fec653422660b1e407656cfc49dbb8ba215
maryorisantosbohorquez <maryori.santos@sierraalta.com.co>
2025-12-04 15:19:10 -0500

    local work

--- ProjectReport/obj/Debug/net8.0-windows/ProjectReport.AssemblyInfo.cs
+++ ProjectReport/obj/Debug/net8.0-windows/ProjectReport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectReport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f34bd2482d13d8de116d2759bb3c1faaaeb4eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectReport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectReport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
